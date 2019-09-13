2b3c8ba524fbb2fb5a35f52a31aefdabf75b28d9
Jan Skrabek <jan.skrabek@tutanota.com>
2019-09-13 19:55:36 +0200

    added rendering

--- dist/js/script.js
+++ dist/js/script.js
@@ -56,10 +56,23 @@
       const thisProduct = this;
       thisProduct.id = id;
       thisProduct.data = data;
+      thisProduct.renderInMenu();
       console.log('new product: ', thisProduct);
     }
-  }
 
+    renderInMenu(){
+      const thisProduct = this;
+      /*generate HTML based on template*/
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      //console.log(generatedHTML);
+      /*create element using utils.createElementFromHTML*/
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      /*find menu container*/
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      /*add element to menu*/
+      menuContainer.appendChild(thisProduct.element);
+    }
+  }
   const app = {
     initMenu: function(){
       const thisApp = this;
